916c5cdc4ce84a199bd25320fc6d21201a803488
mounika <kodatimounika@gmail.com>
2019-04-02 10:29:34 +0530

    this is sample6 project

---
+++ src/test/java/sample6/sample6test.java
@@ -0,0 +1,14 @@
+package sample6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample6test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
